37ffce3e128c5eeb03fb47c457f018c7ac6cadb5
Subhashree Das <subhashree1955das@gmail.com>
2026-09-01 15:47:04 +0530

    updated cors for deployed backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,10 +33,7 @@ async def log_requests(request: Request, call_next):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8081",
-        "http://127.0.0.1:8081",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
