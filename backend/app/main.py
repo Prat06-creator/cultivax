b3511cf5f0e90c9afa980bd48f97f7ee9fcf1dab
Subhashree Das <subhashree1955das@gmail.com>
2026-09-01 10:56:06 +0530

    live chart under Trends page

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,20 @@ from fastapi import FastAPI
 from app.database.connection import Base, engine
 from app.database import models
 from app.api.routes.sensor import router as sensor_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="CultivaX API")
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:8081",
+        "http://127.0.0.1:8081",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
